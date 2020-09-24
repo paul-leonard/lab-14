11196a478702c87879a68dc5c039eb26cdce2f7a
paul-leonard <paul.ryan.leonard@gmail.com>
2020-09-24 12:17:35 -0700

    lab 14 sql normalization complete

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # sql_sandbox
 Practice with SQL
+
+Partners:  Sara Strasner and Paul Leonard
+
+
+
+Number and name of feature: normalize database
+
+Estimate of time needed to complete: 30 minutes
+
+Start time: 10:47
+
+Finish time: 11:19
+
+Actual time needed to complete: 32 minutes
---
+++ data/seed.sql
@@ -0,0 +1,95 @@
+INSERT INTO books (title, author, isbn, image_url, description) VALUES (
+  'Dune',
+  'Frank Herbert',
+  'ISBN_13 9780441013593',
+  'http://books.google.com/books/content?id=B1hSG45JCX4C&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api',
+  'Follows the adven...'
+);
+
+INSERT INTO books (title, author, isbn, image_url, description) VALUES (
+  'What Alice Forgot',
+  'Liane Moriarty',
+  'ISBN_13 1101515376',
+  'http://books.google.com/books/content?id=8iBGzeqj45YC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api',
+  'Alice Lov...'
+);
+
+INSERT INTO books (title, author, isbn, image_url, description) VALUES (
+  'Unsouled',
+  'Will Wight',
+  'ISBN_13 9780989671767',
+  'http://books.google.com/books/content?id=OjYJtAEACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api',
+  'Sacred a...'
+);
+
+INSERT INTO books (title, author, isbn, image_url, description) VALUES (
+  'The Stand',
+  'Stephen King',
+  'ISBN_13 9780385528856',
+  'http://books.google.com/books/content?id=UbfnTcmkaKkC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api',
+  'Stephen King''s apocalyp....'
+);
+
+INSERT INTO books (title, author, isbn, image_url, description) VALUES (
+  'Sweet Thursday',
+  'John Steinbeck',
+  'ISBN_13 978144063549',
+  'http://books.google.com/books/content?id=zer-bEoDL-EC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api',
+  'In Monterey, on the Cal....'
+);
+
+INSERT INTO books (title, author, isbn, image_url, description) VALUES (
+  'The New Jim Crow',
+  'Michelle Alexander',
+  'ISBN_13 9781595588197',
+  'http://books.google.com/books/content?id=reDzBZ3pXqsC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api',
+  'Once in a great whil....'
+);
+
+INSERT INTO books (title, author, isbn, image_url, description) VALUES (
+  'The Boys in the Boat',
+  'Daniel James Brown',
+  'ISBN_13 9781101622742',
+  'http://books.google.com/books/content?id=2p3YPpiA2pEC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api',
+  'For reade....'
+);
+
+INSERT INTO books (title, author, isbn, image_url, description) VALUES (
+  'The Fellowship of the Ring',
+  'John Ronald Reuel Tolkien',
+  'ISBN_13 9780007522903',
+  'http://books.google.com/books/content?id=f3q6mwEACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api',
+  'When they were first pu....'
+);
+
+INSERT INTO books (title, author, isbn, image_url, description) VALUES (
+  'Animal Farm',
+  'George Orwell',
+  'ISBN_13 9780573605383',
+  'http://books.google.com/books/content?id=T0L2nUOPUqAC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api',
+  'Animal Farm is a fab....'
+);
+
+INSERT INTO books (title, author, isbn, image_url, description) VALUES (
+  'The Great Gatsby',
+  'Francis Scott Fitzgerald',
+  'ISBN_13 9786050418156',
+  'http://books.google.com/books/content?id=meVxCwAAQBAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api',
+  'The main events of the nov...'
+);
+
+INSERT INTO books (title, author, isbn, image_url, description) VALUES (
+  'It',
+  'Stephen King',
+  'ISBN_13 9781501141232',
+  'https://books.google.com/books/content?id=-rUACwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api',
+  'It: Chapter Two—now a major mot...'
+);
+
+INSERT INTO books (title, author, isbn, image_url, description) VALUES (
+  'Lord Of The Rings',
+  'John Ronald Reuel Tolkien',
+  'ISBN_13 9780547951942',
+  'https://books.google.com/books/content?id=yl4dILkcqm4C&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api',
+  'A PBS Great Ameri.....'
+);
